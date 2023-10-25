fc941b26bd135b851e126fd0fdec407ef9946842
DorianBadel <dorianbadel@protonmail.com>
2023-10-25 18:53:07 +0200

    Creating global styles file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles/css/Global.css";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
+          <Route
+            path="/"
+            element={<div className="heading__text-l">Home</div>}
+          />
         </Routes>
       </Router>
     </>
